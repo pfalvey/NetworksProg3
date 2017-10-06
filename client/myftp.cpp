c03d6e777e21a3d9db22a70bde1bd2ec0c5ed27a
Matthew Taylor Flanagan <mflanag6@student01.cse.nd.edu>
2017-10-06 15:10:53 -0400

    Fixed some formatting in client

--- client/myftp.cpp
+++ client/myftp.cpp
@@ -17,18 +17,23 @@ main(int argc, char * argv[])
     char buf[MAX_LINE];
     int s;
     int len;
-    if (argc==2) {
+    if (argc==2) 
+    {
         host = argv[1];
     }
-    else {
+    else 
+    {
         fprintf(stderr, "usage: simplex-talk host\n");
-        exit(1);}
+        exit(1);
+    }
 
     /* translate host name into peer's IP address */
     hp = gethostbyname(host);
-    if (!hp) {
+    if (!hp) 
+    {
         fprintf(stderr, "simplex-talk: unknown host: %s\n", host);
-        exit(1);}
+        exit(1);
+    }
 
     /* build address data structure */
     bzero((char *)&sin, sizeof(sin));
@@ -36,23 +41,35 @@ main(int argc, char * argv[])
     bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
     sin.sin_port = htons(SERVER_PORT);
     /* active open */
-    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("simplex-talk: socket"); exit(1);}
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) 
+    {
+        perror("simplex-talk: socket"); exit(1);
+    }
+    
     printf("Welcome to your first TCP client! To quit, type \'Exit\'\n");
 
     if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
-    {perror("simplex-talk: connect");
-        close(s); exit(1);}
+    {
+      perror("simplex-talk: connect");
+      close(s); exit(1);
+    }
 
     /* main loop: get and send lines of text */
-    while (fgets(buf, sizeof(buf), stdin)) {
+    while (fgets(buf, sizeof(buf), stdin)) 
+    {
         buf[MAX_LINE-1] = '\0';
-        if (!strncmp(buf, "Exit",4)){
+        if (!strncmp(buf, "Exit",4))
+	{
             printf("Good Bye!\n");
-            break;}
+            break;
+	}
 
         len = strlen(buf) + 1;
-        if(send(s, buf, len, 0)==-1){
-            perror("client send error!"); exit(1);}}
-    close(s);}
+        if(send(s, buf, len, 0)==-1)
+	{
+            perror("client send error!"); exit(1);
+	}
+    }
+    close(s);
+}
 
