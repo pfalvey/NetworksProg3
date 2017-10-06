e1699e98476e3e90c81881fd9868c57caf6bf3e3
Matthew Taylor Flanagan <mflanag6@student01.cse.nd.edu>
2017-10-06 15:13:33 -0400

    Fix formatting in server

--- server/myftpd.cpp
+++ server/myftpd.cpp
@@ -8,8 +8,8 @@
 #define SERVER_PORT 41700
 #define MAX_PENDING 5
 #define MAX_LINE 256
-    int
-main()
+    
+int main()
 {
     struct sockaddr_in sin;
     char buf[MAX_LINE];
@@ -21,35 +21,47 @@ main()
     sin.sin_addr.s_addr = INADDR_ANY;
     sin.sin_port = htons(SERVER_PORT);
     /* setup passive open */
-    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) 
+    {
         perror("simplex-talk: socket");
         exit(1);
     }
     // set socket option
-    if ((setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *)& opt, sizeof(int)))<0){
+    if ((setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *)& opt, sizeof(int)))<0)
+    {
         perror ("simplex-talk:setscokt");
         exit(1);
     }
-    if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
+    if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) 
+    {
         perror("simplex-talk: bind"); exit(1);
     }
-    if ((listen(s, MAX_PENDING))<0){
+    if ((listen(s, MAX_PENDING))<0)
+    {
         perror("simplex-talk: listen"); exit(1);
     } 
     printf("Welcome to the first TCP Server!\n");
     /* wait for connection, then receive and print text */
-    while(1) {
-        if ((new_s = accept(s, (struct sockaddr *)&sin, &len)) < 0) {
+    while(1) 
+    {
+        if ((new_s = accept(s, (struct sockaddr *)&sin, &len)) < 0) 
+	{
             perror("simplex-talk: accept");
-            exit(1);}
+            exit(1);
+	}
 
-        while (1){
-            if((len=recv(new_s, buf, sizeof(buf), 0))==-1){
+        while (1)
+	{
+            if((len=recv(new_s, buf, sizeof(buf), 0))==-1)
+	    {
                 perror("Server Received Error!");
-                exit(1);}
+                exit(1);
+	    }
             if (len==0) break;
-            printf("TCP Server Received:%s", buf);}
+            printf("TCP Server Received:%s", buf);
+	}
         printf("Client finishes, close the connection!\n");
         close(new_s);
-    }}
+    }
+}
 
